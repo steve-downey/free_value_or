8ebb57687d173cb52c95c8bfe35521001c184786
Steve Downey <sdowney@gmail.com>
2025-03-15 10:28:13 -0400

    value_or should remove_cv_t

--- optional.hpp
+++ optional.hpp
@@ -1181,7 +1181,7 @@ class optional<T&> {
     constexpr bool     has_value() const noexcept;
     constexpr T&       value() const;
     template <class U>
-    constexpr std::remove_cv_t<T> value_or(U&& u) const;
+    constexpr T value_or(U&& u) const;
 
     // \ref{optionalref.monadic}, monadic operations
     template <class F>
@@ -1341,10 +1341,10 @@ constexpr T& optional<T&>::value() const {
 
 template <class T>
 template <class U>
-constexpr std::remove_cv_t<T> optional<T&>::value_or(U&& u) const {
+constexpr T optional<T&>::value_or(U&& u) const {
     static_assert(std::is_constructible_v<std::remove_cv_t<T>, T&>, "T must be constructible from a T&");
     static_assert(std::is_convertible_v<U, std::remove_cv_t<T>>, "Must be able to convert u to T");
-    return has_value() ? std::remove_cv_t<T>(*value_) : std::forward<U>(u);
+    return has_value() ? *value_ : std::forward<U>(u);
 }
 
 //   \rSec3[optionalref.monadic]{Monadic operations}
