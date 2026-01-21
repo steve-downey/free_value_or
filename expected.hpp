6d102f41a8e06d16674dc5cb5513b0625a38c799
Steve Downey <sdowney@gmail.com>
2026-01-21 08:14:59 -0500

    Remove identity and add expected
    Remove the identity component
    Add Expected and Unexpected
    Add failing tests
    Add empty example

---
+++ expected.hpp
@@ -0,0 +1,12 @@
+// beman/expected/expected.hpp                                        -*-C++-*-
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#ifndef BEMAN_EXPECTED_EXPECTED_HPP
+#define BEMAN_EXPECTED_EXPECTED_HPP
+
+namespace beman {
+namespace expected {
+
+}
+} // namespace beman
+
+#endif
