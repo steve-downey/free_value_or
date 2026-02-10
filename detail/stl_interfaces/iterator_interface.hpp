543dc1ab2e6a933bc0842f59e5857014d156af42
Durly <andrei.durlea.telefon@gmail.com>
2026-02-10 21:14:58 +0200

    Applied file.copyright from the beman standard

--- detail/stl_interfaces/iterator_interface.hpp
+++ detail/stl_interfaces/iterator_interface.hpp
@@ -1,8 +1,6 @@
 // include/beman/optional/detail/stl_interfaces/iterator_interface.hpp -*-C++-*-
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-// Copyright (C) 2019 T. Zachary Laine
-//
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
