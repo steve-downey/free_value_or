84bc41f9b0b4be79658b6681eb530e8b049b592f
Durly <andrei.durlea.telefon@gmail.com>
2026-02-11 18:53:06 +0200

    Revert "Applied file.copyright from the beman standard"
    This reverts commit 23800d960bc7ce05784f1fbc2ae103838c1a9815.

--- detail/stl_interfaces/config.hpp
+++ detail/stl_interfaces/config.hpp
@@ -1,6 +1,8 @@
 // include/beman/optional/detail/stl_interfaces/config.hpp            -*-C++-*-
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+// Copyright (C) 2020 T. Zachary Laine
+//
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
